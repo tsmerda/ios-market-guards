cbe75a61265dd17fc19b15aef1f507bac5601da4
tsmerda <tsmerda2@gmail.com>
2020-09-27 18:52:15 +0200

    Initial Commit

---
+++ MarketGuards/MarketGuardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketGuardsApp.swift
+//  MarketGuards
+//
+//  Created by Tomáš Šmerda on 27/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct MarketGuardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
